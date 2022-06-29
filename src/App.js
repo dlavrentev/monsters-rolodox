32ee47aa7dc96fbfd8cca6ccd6dccd1ed4320d9b
dlavrentev <dimitrilavrentev96@gmail.com>
2022-06-29 16:19:19 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import {Component} from "react";
-import logo from './logo.svg';
 import './App.css';
-import { render } from "@testing-library/react";
 
 class App extends Component {
   constructor() {
@@ -10,9 +8,11 @@ class App extends Component {
     this.state = {
       monsters: [],
     };
+    console.log('constructor')
   }
 
   componentDidMount() {
+    console.log('componentDidMount')
     fetch('https://jsonplaceholder.typicode.com/users')
     .then((response) => response.json())
     .then((users) => this.setState(
@@ -26,8 +26,16 @@ class App extends Component {
   }
 
   render() {
+    console.log('render')
   return (
     <div className="App">
+      <input className="search-box"
+        type='search'
+        placeholder='search monsters'
+        onChange={(event) => {
+           console.log(event.target.value);
+      }}
+      />
       {this.state.monsters.map((monster) => {
           return (
                   <div key={monster.id}>
