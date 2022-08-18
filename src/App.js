c499eab7bc8dd7a73b1f933612226f17ce74c89b
Dimitri Lavrentev <dimitrilavrentev96@gmail.com>
2022-08-18 11:52:32 +0200

    updated

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import {Component} from "react";
-import './App.css';
+import { Component } from "react";
+import "./App.css";
 
 class App extends Component {
   constructor() {
@@ -8,52 +8,60 @@ class App extends Component {
     this.state = {
       monsters: [],
     };
-    console.log('constructor')
+    console.log("constructor");
   }
 
   componentDidMount() {
-    console.log('componentDidMount')
-    fetch('https://jsonplaceholder.typicode.com/users')
-    .then((response) => response.json())
-    .then((users) => this.setState(
-      () => {
-      return { monsters: users };
-    },
-    () => {
-      console.log(this.state);
-    }
-    ));
+    console.log("componentDidMount");
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) =>
+        this.setState(
+          () => {
+            return { monsters: users };
+          },
+          () => {
+            console.log(this.state);
+          }
+        )
+      );
   }
 
   render() {
-    console.log('render')
-  return (
-    <div className="App">
-      <input className="search-box"
-        type='search'
-        placeholder='search monsters'
-        onChange={(e) => {
-           console.log(e.target.value);
-           const searchString = e.target.value.toLocaleLowerCase();
-           // [ { name: 'Leanne'}, {name: 'Yihua'} ]
-           const filteredMonsters = this.state.monsters.filter((monster) => {
-            return monster.name.toLocaleLowerCase().includes(e.target.value);
-           });
+    console.log("render");
+    return (
+      <div className="App">
+        
+        <input
+          className="search-box"
+          type="search"
+          placeholder="search monsters"
+          onChange={(e) => {
+            console.log(e.target.value);
+            const searchString = e.target.value.toLocaleLowerCase();
+            // [ { name: 'Leanne'}, {name: 'Yihua'} ]
+            const filteredMonsters = this.state.monsters.filter((monster) => {
+              return monster.name.toLocaleLowerCase().includes(e.target.value);
+            });
 
-           this.setState(() => {
-            return { monsters: filteredMonsters }
-           })
-      }}
-      />
-      {this.state.monsters.map((monster) => {
+            this.setState(() => {
+              return { monsters: filteredMonsters };
+            });
+          }}
+        />
+
+
+        {this.state.monsters.map((monster) => {
           return (
-                  <div key={monster.id}>
-                    <h1>{monster.name}</h1>
-                  </div>
+            <div key={monster.id}>
+              <h1>{monster.name}</h1>
+            </div>
           );
         })}
-    </div>
-  );
+
+
+      </div>
+    );
   }
 }
 
